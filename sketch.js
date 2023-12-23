b7091309a68dea6db089419cfae32fd4118fbc96
haldun <haldunbucak97@outlook.com>
2023-12-23 17:41:29 +0100

    Made the function smooth

--- sketch.js
+++ sketch.js
@@ -12,15 +12,15 @@ let field;
 function setup() {
 	// Hyperparameters
   HEADER_OFFSET = 57;
-	size = 50;
+	size = 20;
 
 	// Calculate window sizes
   w = windowWidth;
   h = windowHeight - HEADER_OFFSET;
 
 	// Calculate the row and column size
-	rows = int(h / size);
-	columns = int(w / size);
+	rows = int(h / size) + 1;
+	columns = int(w / size) + 1;
 
 	// Initialize the Scalar Field
 	field = new ScalarField(rows, columns, size);
@@ -30,6 +30,6 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  background(0);
+  background("#3C2C4F");
 	field.draw();
 }
